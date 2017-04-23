56f1aaee844a339063879e31a316d5a9f1b13fb7
Miggus Studio <support@pontius-pilatus-records.com>
2017-04-24 00:08:23 +0200

    changed grid and exported cell to seperate scriptfile

--- sketch.js
+++ sketch.js
@@ -21,16 +21,3 @@ function draw() {
       grid[i].show();
     }
 }
-
-function Cell(i, j){
-    this.i = i;
-    this.j = j;
-
-    this.show = function(){
-      var x = this.i*w;
-      var y = this.j*w;
-      stroke(255);
-      noFill();
-      rect(x,y,w,w);
-    }
-}

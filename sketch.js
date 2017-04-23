5695734e5f502bce68d46fcb5f6611637e74c5cf
Miggus Studio <support@pontius-pilatus-records.com>
2017-04-23 23:53:47 +0200

    added grid

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var rows, cols;
+var w = 40;
+var grid = [];
+
+function setup() {
+    createCanvas(400, 400);
+    cols = floor(width/w);
+    rows = floor(height/w);
+
+    for (var j = 0; j < rows; j++){
+      for (var i = 0; i < cols; i++){
+        var cell = new Cell(i,j);
+        grid.push(cell);
+      }
+    }
+}
+
+function draw() {
+    background(51);
+    for (var i = 0; i < grid.length; i++){
+      grid[i].show();
+    }
+}
+
+function Cell(i, j){
+    this.i = i;
+    this.j = j;
+
+    this.show = function(){
+      var x = this.i*w;
+      var y = this.j*w;
+      stroke(255);
+      noFill();
+      rect(x,y,w,w);
+    }
+}

9ea63b54c1b76b7e310935f06cc537637f55a4d7
Miggus Studio <support@pontius-pilatus-records.com>
2017-04-24 00:28:43 +0200

    added moving algorithm

--- sketch.js
+++ sketch.js
@@ -2,22 +2,36 @@ var rows, cols;
 var w = 40;
 var grid = [];
 
-function setup() {
-    createCanvas(400, 400);
-    cols = floor(width/w);
-    rows = floor(height/w);
+var current;
 
-    for (var j = 0; j < rows; j++){
-      for (var i = 0; i < cols; i++){
-        var cell = new Cell(i,j);
-        grid.push(cell);
-      }
+function setup() {
+  createCanvas(400, 400);
+  cols = floor(width / w);
+  rows = floor(height / w);
+
+  frameRate(5);
+
+  for (var j = 0; j < rows; j++) {
+    for (var i = 0; i < cols; i++) {
+      var cell = new Cell(i, j);
+      grid.push(cell);
     }
+  }
+
+  current = grid[0];
 }
 
 function draw() {
-    background(51);
-    for (var i = 0; i < grid.length; i++){
-      grid[i].show();
-    }
+  background(51);
+  for (var i = 0; i < grid.length; i++) {
+    grid[i].show();
+  }
+
+  current.visited = true;
+  var next = current.checkNeighbors();
+
+  if (next) {
+    next.visited = true;
+    current = next;
+  }
 }
